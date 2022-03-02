6e889d912b274b98af3a8297b141635c8e92e65f
uday <uday@mobifyi.com>
2022-03-02 22:17:02 +0530

    basic setup

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Uday on 01/03/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
