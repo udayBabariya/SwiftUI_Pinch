f7e6493bf6ce755660b36a6c4b0041bb6349bfb4
uday <uday@mobifyi.com>
2022-03-08 22:28:22 +0530

    feat - info panel view with long press gesture

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -27,6 +27,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             ZStack{
+                Color.clear
                 //MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -69,6 +70,13 @@ struct ContentView: View {
                     isAnimating = true
                 }
             }
+            //MARK: INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                , alignment: .top
+            )
         }//: NAVIGATION
         .navigationViewStyle(.stack)
     }
